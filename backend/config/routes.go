43a1929d1442345d8c3e631fdbb54b31c7a42b30
Krunoslav Husak <kruno@husak.me>
2024-06-08 23:22:00 +0200

    POST method for grammar check route

--- backend/config/routes.go
+++ backend/config/routes.go
@@ -6,7 +6,7 @@ func Routes() raptor.Routes {
 	return raptor.CollectRoutes(
 		raptor.Scope("/api/v1",
 			raptor.Scope("/grammar",
-				raptor.Route("GET", "/check", "GrammarController", "Check"),
+				raptor.Route("POST", "/check", "GrammarController", "Check"),
 			),
 		),
 	)
